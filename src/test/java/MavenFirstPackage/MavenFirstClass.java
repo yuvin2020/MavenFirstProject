c325b130f3b7834b5fb6ce232396fff609143377
Dharma <Dharma@LAPTOP-C4RGVJG3>
2021-03-03 00:27:19 +0530

    This is a sample Maven project upload to GitHub

---
+++ src/test/java/MavenFirstPackage/MavenFirstClass.java
@@ -0,0 +1,19 @@
+package MavenFirstPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenFirstClass 
+{
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\Cucumber Setup\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.co.in");
+		
+		System.out.println("Browser is launched");
+
+	}
+
+}
